affa786b4ca633a913929afad1bd6296575f97d4
Niklas Lindell <niklas_lindell@hotmail.com>
2019-03-18 16:20:19 +0100

    Initial Commit

---
+++ ShortcutTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  ShortcutTest
+//
+//  Created by Niklas Lindell on 2019-03-18.
+//  Copyright © 2019 Niklas Lindell. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
